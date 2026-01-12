127781d67da6c7f2e1cc4dfea02f56c308398127
takabatake2911 <chph53.hiro@gmail.com>
2026-01-12 02:59:28 +0000

    Parsing the line

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 #define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 
 char *lsh_read_line(void)
 {
@@ -43,7 +45,36 @@ char *lsh_read_line(void)
 
 char **lsh_split_line(char *line)
 {
-    // NOT IMPLEMENTED
+    int bufsize = LSH_TOK_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+    if (!tokens)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
 }
 
 int lsh_execute(char **args)
