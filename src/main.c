4e5cfcc57a33d5a655b161593d4f74d5d3d07d96
takabatake2911 <chph53.hiro@gmail.com>
2026-01-12 01:31:42 +0000

    Basic loop of a shell

--- src/main.c
+++ src/main.c
@@ -1,6 +1,37 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-void lsh_loop(void) {}
+char *lsh_read_line()
+{
+    // NOT IMPLEMENTED
+}
+
+char **lsh_split_line(char *line)
+{
+    // NOT IMPLEMENTED
+}
+
+int lsh_execute(char **args)
+{
+    // NOT IMPLEMENTED
+}
+
+void lsh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+    do
+    {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
 
 int main()
 {
