95de22424227e3c7872bf51803de3e988b2a9d50
takabatake2911 <chph53.hiro@gmail.com>
2026-01-12 04:47:29 +0000

    bug fix

--- src/main.c
+++ src/main.c
@@ -54,7 +54,7 @@ int lsh_cd(char **args)
     return 1;
 }
 
-int lsh_help(char **args)
+int lsh_help(char ** /* args */)
 {
     int i;
     printf("Stephen Brennan's LSH\n");
@@ -69,7 +69,7 @@ int lsh_help(char **args)
     return 1;
 }
 
-int lsh_exit(char **args)
+int lsh_exit(char ** /* args */)
 {
     return 0;
 }
@@ -146,27 +146,9 @@ char **lsh_split_line(char *line)
     return tokens;
 }
 
-int lsh_execute(char **args)
-{
-    int i;
-    if (args[0] == NULL)
-    {
-        return 1;
-    }
-    for (i = 0; i < lsh_num_builtins(); i++)
-    {
-        if (strcmp(args[0], builtin_str[i]) == 0)
-        {
-            return (*builtin_func[i])(args);
-        }
-    }
-
-    return lsh_launch(args);
-}
-
 int lsh_launch(char **args)
 {
-    pid_t pid, wpid;
+    pid_t pid;
     int status;
     pid = fork();
     if (pid == 0)
@@ -185,10 +167,28 @@ int lsh_launch(char **args)
     {
         do
         {
-            wpid = waitpid(pid, &status, WUNTRACED);
+            waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
     return 1;
+}
+
+int lsh_execute(char **args)
+{
+    int i;
+    if (args[0] == NULL)
+    {
+        return 1;
+    }
+    for (i = 0; i < lsh_num_builtins(); i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
 }
 
 void lsh_loop(void)
