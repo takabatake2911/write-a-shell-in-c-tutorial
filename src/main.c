bef3fee3c5648b7d430daa408f0a62a1f79de6df
takabatake2911 <chph53.hiro@gmail.com>
2026-01-12 01:54:34 +0000

    Reading a line

--- src/main.c
+++ src/main.c
@@ -1,9 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char *lsh_read_line()
+#define LSH_RL_BUFSIZE 1024
+
+char *lsh_read_line(void)
 {
-    // NOT IMPLEMENTED
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c; // Use an int to check for EOF.
+    if (!buffer)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    while (1)
+    {
+        c = getchar();
+        if (c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
+            {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 }
 
 char **lsh_split_line(char *line)
